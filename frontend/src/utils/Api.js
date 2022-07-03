0dddc94c31dde88e80a91b005ed362b3315e51e9
minseok <kms9887@naver.com>
2022-07-03 22:53:09 +0900

    Add file for API API들 공통으로 모아둔 파일 현재 로그인, 회원가입 기능 추가

---
+++ frontend/src/utils/Api.js
@@ -0,0 +1,40 @@
+const url = process.env.REACT_APP_URL;
+
+export const signUp = async (email, password) => {
+  console.log(url);
+  const res = await fetch(url + '/signup', {
+    method: "POST",
+    body: JSON.stringify({
+      email,
+      password
+    })
+  });
+  if (res.status === 409) {
+    alert("이미 존재하는 이메일입니다.")
+  } else {
+    const data = await res.json();
+    alert("회원가입이 완료되었습니다. 로그인 후 이용가능합니다.");
+  }
+};
+
+export const signIn = async (email, password) => {
+  const res = await fetch(url + '/signin', {
+    method: "POST",
+    headers: {
+      "Content-Type":"application/json"
+    },
+    body: JSON.stringify({
+      email,
+      password
+    })
+  });
+  if (res.status === 409) {
+    alert("존재하지 않거나 비밀번호가 다릅니다.")
+  } else {
+    const data = await res.json();
+    localStorage.setItem("jwt", data.authorization);
+    const isAdmin = data.isAdmin ? 1 : 0;
+    localStorage.setItem("admin", isAdmin);
+    window.location.href = "/";
+  }
+};
