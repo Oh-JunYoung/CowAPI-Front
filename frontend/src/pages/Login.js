07a7fa57278db0aa8cbc9a5619ffec5e59e1e390
minseok <kms9887@naver.com>
2022-07-03 22:53:33 +0900

    Feat Login 로그인 화면 및 기능 구현 소셜 로그인 기능은 아직 미구현

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,12 +1,77 @@
+import { useState } from "react";
+import styled from "styled-components";
+import Button from "../components/Button";
+
 import NavigationBar from "../components/NavigationBar";
 
+import { signIn, signUp } from "../utils/Api";
+
 const Login = () => { 
+  if (localStorage.getItem("jwt") !== null) { 
+    window.location.href = "/"; 
+  }
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
   return (
     <>
       <NavigationBar />
-      <div>로그인</div>
+      <LoginContainer>
+        <EpContainer>
+          <InputBox>
+            <TextBox>Email</TextBox>
+            <Input onChange={(e) => { 
+              setEmail(e.target.value);
+            }}/>
+            <Button func={() => { signIn(email, password)}} name="로그인"/>
+          </InputBox>
+          <InputBox>
+            <TextBox>Password</TextBox>
+            <Input onChange={(e) => {
+              setPassword(e.target.value);
+            }} />
+            <Button func={() => { signUp(email, password) }} name="회원가입" />
+            </InputBox>
+        </EpContainer>
+        <InputBox>
+          <TextBox>소셜 계정 로그인</TextBox>
+          <Button func={() => console.log("네이버")} name="NAVER" />
+        </InputBox>
+      </LoginContainer>
     </>
   );
 }
 
 export default Login;
+
+const LoginContainer = styled.div`
+  margin-top: 8vh;
+  margin-left : 15vw;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  height : 60vh;
+  width : 70vw;
+`;
+
+const EpContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const InputBox = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  margin: 30px;
+`;
+
+const TextBox = styled.div`
+  min-width : 120px;
+  font-size : 25px;
+`
+
+const Input = styled.input`
+  width : 400px;
+  font-size : 25px;
+`
