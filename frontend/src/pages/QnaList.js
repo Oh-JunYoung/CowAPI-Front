bd00d28975a60225697a7e49e16620a048a5626f
minseok <kms9887@naver.com>
2022-07-04 13:58:06 +0900

    Add QnaList 질문 리스트 화면 구현중 아직 완료 안됨

--- frontend/src/pages/QnaList.js
+++ frontend/src/pages/QnaList.js
@@ -1,12 +1,26 @@
-import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { useParams, useNavigate } from "react-router-dom";
 import NavigationBar from "../components/NavigationBar";
+import { Button, Title, Container } from "../components/Component";
 
 const QnaList = (props) => { 
-  //console.log(useParams().page)
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (localStorage.getItem("jwt") === null) {
+      alert("로그인 후 이용 바랍니다.");
+      navigate(-1);
+    }
+  });
+
+  const [page, setPage] = useState(useParams().page);
+
+  
+
   return (
     <>
       <NavigationBar />
-      <div>질문리스트</div>
+      <Title name="QnA"/>
+      <Container>질문리스트</Container>
     </>
   );
 }
