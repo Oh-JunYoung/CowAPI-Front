5a062cbe4c6ef23c0b8d94f44e49fb936b1d2a09
minseok <kms9887@naver.com>
2022-07-04 21:41:21 +0900

    Update Component for Qna,Notice List qna, 공지 리스트 표현을 위한 컴포넌트 추가 qna와 공지 모두 같은 컴포넌트 사용(props로 세부 내용 조절)

--- frontend/src/components/Component.js
+++ frontend/src/components/Component.js
@@ -19,13 +19,74 @@ export const Container = styled.div`
   margin-left: 40px;
   margin-right: 40px;
   margin-bottom : 20px;
-  background-color: yellow;
   height : 70vh;
 `
 
-export const Board = styled.div`
-  
+export const BoardContainer = (props) => { 
+  return (
+    <Bd>
+      <One style={{height:"10%"}}>
+        <QnaNo>No.</QnaNo>
+        <QnaTitle>제목</QnaTitle>
+        <QnaDate>날짜</QnaDate>
+        <QnaUser>사용자</QnaUser>
+      </One>
+      {
+        props.list.map((li) => { 
+          return (
+            <One key={li.id}>
+              <QnaNo>{li.id}</QnaNo>
+              <QnaTitle>{li.title}</QnaTitle>
+              <QnaDate>{li.updatedAt.substr(0,10)}</QnaDate>
+              <QnaUser>{li.email}</QnaUser>
+            </One>
+          );
+        })
+      }
+    </Bd>
+  );
+}
+
+export const ButtonContainer = styled.div`
+  display: flex;
+  justify-content: ${(props) => props.justify};
+  margin-top : 20px;
+  min-height : 38px;
 `
+
+export const PageContainer = (props) => { 
+  const cur = props.cur;
+  const start = parseInt(props.cur / 5) * 5 + 1 - (props.cur % 5 === 0 ? 5 : 0);
+  const last = props.last > start + 4 ? start + 4 : props.last;
+  
+  const arr = ['<<'];
+  for (let i = start; i <= last; i++)
+    arr.push(i);
+  arr.push('>>');
+  return (
+    <ButtonContainer justify="space-around">
+      {
+        arr.map((p) => {
+          return (
+            <PageNo key={p}
+              style={cur == p ? { fontWeight: "bolder" } : { fontWeight: "normal" }}
+              onClick={(e) => { 
+                if (e.target.innerText == '<<') {
+                  if(start - 1 > 0)
+                    window.location.href = `./${start - 1}`;
+                } else if (e.target.innerText == '>>') {
+                  if(last + 1 <= props.last)
+                    window.location.href = `./${last + 1}`;
+                } else
+                  window.location.href = `./${e.target.innerText}`;
+              }}
+            >{p}</PageNo>
+          );
+        })
+      }
+    </ButtonContainer>
+  );
+}
 
 const Btn = styled.div`
   display : flex;
@@ -48,5 +109,63 @@ const Tit = styled.div`
 `
 
 const Bd = styled.div`
+  //border: black 1px solid;
+  height: 70%;
+`
 
+const One = styled.div`
+  display: flex;
+  flex-direction : row;
+  height : 18%;
+  margin-top: -1px;
+`
+
+const QnaNo = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  border: black 1px solid;
+  width: 10%;
+  text-align:center;
+  font-size: x-large;
+  margin-left: -1px;
+`
+
+const QnaTitle = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  border: black 1px solid;
+  width: 50%;
+  text-align:center;
+  font-size: x-large;
+  margin-left: -1px;
+`
+
+const QnaDate = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  border: black 1px solid;
+  width: 20%;
+  text-align:center;
+  font-size: x-large;
+  margin-left: -1px;
+`
+
+const QnaUser = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  border: black 1px solid;
+  width: 20%;
+  text-align:center;
+  font-size: x-large;
+  margin-left: -1px;
+  margin-right:-1px;
+`
+
+const PageNo = styled.div`
+  font-size: x-large;
+  margin : 10px;
 `
