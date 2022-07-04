a22e064da9659e8cfe734b9169d43419f5356177
minseok <kms9887@naver.com>
2022-07-04 13:57:44 +0900

    Update Login 로그인 되어있는 경우 이전 페이지로 redirect

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -11,7 +11,7 @@ const Login = () => {
   const navigate = useNavigate();
   useEffect(() => {
     if (localStorage.getItem("jwt") !== null) {
-      navigate("/");
+      navigate(-1);
     }
   });
 
