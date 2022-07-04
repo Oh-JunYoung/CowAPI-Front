433230deab3c0a0a41c8f453ac756daad80c4fad
dssong <twghttwght@naver.com>
2022-07-04 14:55:27 +0900

    AI LIST dummy version

---
+++ frontend/src/components/AiTable.js
@@ -0,0 +1,79 @@
+import styled from "styled-components";
+
+export const AiTable = ({ lists }) => {
+  return (
+    <Block>
+      <TableStyles>
+        <thead>
+          <tr>
+            <th>AI</th>
+            <th>Response Time (ms)</th>
+            <th>Accuracy</th>
+          </tr>
+        </thead>
+        <tbody>
+          {lists.map((el, index) => (
+            <tr key={index}>
+              <td>{el.name}</td>
+              <td>{el.responseTime}</td>
+              <td>{el.accuracy}</td>
+            </tr>
+          ))}
+        </tbody>
+      </TableStyles>
+    </Block>
+  );
+};
+
+const Block = styled.div`
+  overflow-y: scroll;
+  max-height: 162px;
+  height: 100%;
+  width: 90%;
+  margin-top: 18px;
+`;
+
+const TableStyles = styled.table`
+  width: 100%;
+  border-collapse: collapse;
+  background-color: #f8f8f8;
+
+  thead {
+    position: sticky;
+    top: 0;
+    background-color: #f8f8f8;
+  }
+
+  th,
+  td {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border: 1px solid black;
+  }
+
+  th {
+    color: #464646;
+    font-size: 14px;
+    align-items: center;
+    justify-content: center;
+    font-weight: 400;
+    flex: 1;
+  }
+
+  tr {
+    height: 40px;
+    display: flex;
+  }
+  td {
+    flex: 1;
+    background-color: #fff;
+    font-size: 14px;
+    color: #464646;
+    align-items: center;
+    display: flex;
+    justify-content: center;
+    font-weight: 400;
+    border-bottom: 1px solid #ececec;
+  }
+`;
